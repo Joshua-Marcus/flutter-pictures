288450576c8b78e1599af62bbc2ccde4c1ac2378
Joshua Marcus <joshuamarcuswebdesign@gmail.com>
2020-02-25 15:13:27 +1030

    app layout

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,16 @@ import 'package:flutter/material.dart';
 void main() { 
 
   var app = MaterialApp(
-    home: Text('Hi There!'),
+    home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Button Pressed');
+        },
+      ),
+      appBar: AppBar(
+        title: Text('Flutter Images!'),
+      ),
+    ),
   );
 
 runApp(app);
