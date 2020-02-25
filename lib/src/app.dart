e5799b384bb06811c1d07cbb23d80b81a5dc5779
Joshua Marcus <joshuamarcuswebdesign@gmail.com>
2020-02-25 22:22:24 +1030

    added theme data + stateful widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
-    theme: ThemeData(
-      brightness: Brightness.dark,
-      primaryColor: Colors.purple
-    ),
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.purple,
-        child: Icon(Icons.add, size: 35),
-        onPressed: () {
-          print('Button Pressed');
-        },
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.purple,
       ),
-      appBar: AppBar(
-        title: Text('Flutter Images!'),
+      home: Scaffold(
+        body: Text('$counter',),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.purple,
+          child: Icon(Icons.add, size: 35),
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+            print(counter);
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Flutter Images!'),
+        ),
       ),
-    ),
-  );
+    );
   }
 }
